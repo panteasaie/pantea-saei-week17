779e182c138b8fb18d6826585b7be3ce8573f3e3
Pantea Saei <pantisa2020@gmail.com>
2025-11-24 10:17:55 -0800

    feat:Improve contact edit workflow and validation

--- src/validation/ContactsSchema.jsx
+++ src/validation/ContactsSchema.jsx
@@ -1,19 +1,19 @@
 import * as yup from "yup";
 const schema = yup.object({
-  name: yup.string().min(2, "حداقل 2 کارکتر!").required("نام الزامیست"),
+  name: yup.string().min(2,"Name must be at least 2 characters!").required("Name is required!"),
   lastName: yup
     .string()
-    .min(2, "حداقل دو کارکتر")
-    .required("نام خانوادگی الزامیست"),
+    .min(2, "Last name must be at least 2 characters!")
+    .required("Last name is required!"),
   email: yup
     .string()
-    .email("فرمت ایمیل نامعتبر")
-    .required("ایمیل الزامیست")
-    .test("unique-email", "ایمیل تکراریست", function (value) {
+    .email("Invalid email format!")
+    .required("Email is required!")
+    .test("unique-email", "Email already exists!", function (value) {
       const { contacts, currentId } = this.options.context;
       if (!contacts) return true;
       return !contacts.some((c) => c.email === value && c.id !== currentId);
     }),
-  phone: yup.string().min(11, "شماره نامعتبر").required("شماره تلفن الزامیست"),
+  phone: yup.string().min(11, "Phone number must be at least 11 digits").required("Phone number is required!"),
 });
 export default schema;
